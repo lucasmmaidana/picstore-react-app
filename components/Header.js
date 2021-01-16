7db47924255b9a2b86859c176344adbc6e9f702a
lucasmmaidana <lucasmaidanasoy@gmail.com>
2021-01-16 17:39:30 -0300

    Fixes

--- components/Header.js
+++ components/Header.js
@@ -1,19 +1,24 @@
-import React, {useContext} from "react"
-import {Link} from "react-router-dom"
+import React, { useContext } from "react"
+import { Link } from "react-router-dom"
 
-import {Context} from "../Context"
+import { Context } from "../Context"
 
 function Header() {
-    const {cartItems} = useContext(Context)
-    const cartClassName = cartItems.length > 0 ? "ri-shopping-cart-fill" : "ri-shopping-cart-line"
-    return (
-        <header>
-            <Link to="/"><h2>Pic Some</h2></Link>
-            <Link to="/cart">
-                <i className={`${cartClassName} ri-fw ri-2x`}></i>
-            </Link>
-        </header>
-    )
+  const { cartItems } = useContext(Context)
+  const cartClassName =
+    cartItems.length > 0 ? "ri-shopping-cart-fill" : "ri-shopping-cart-line"
+  return (
+    <header>
+      <div className="header-content">
+        <Link to="/">
+          <h2>PicStore</h2>
+        </Link>
+        <Link to="/cart">
+          <i className={`${cartClassName} ri-fw ri-2x`}></i>
+        </Link>
+      </div>
+    </header>
+  )
 }
 
 export default Header
